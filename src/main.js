49f3ebd15159697ea2a5b47bee163e7c01ac73d4
Adam <adamskoullos@gmail.com>
2021-03-10 14:33:41 +0000

    main.js - firebase auth beofre loading

--- src/main.js
+++ src/main.js
@@ -4,4 +4,15 @@ import router from './router'
 
 import './assets/main.css'
 
-createApp(App).use(router).mount('#app')
+// firebase auth service
+import { fAuth } from './firebase/config'
+
+let app
+
+fAuth.onAuthStateChanged(() => {
+    if(!app){
+        app = createApp(App).use(router).mount('#app')
+    }
+})
+
+
