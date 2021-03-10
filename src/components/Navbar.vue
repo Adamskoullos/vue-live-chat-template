c5852d0b9c221bef3bd6c2bf4dfd37330df69299
Adam <adamskoullos@gmail.com>
2021-03-10 16:18:26 +0000

    redirect on logout

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -11,17 +11,14 @@
 <script>
 import useLogout from '../composables/logout'
 import getUser from '../composables/getUser'
-import { useRouter } from 'vue-router'
 
 export default {
 
     setup(){
         const { error, logout } = useLogout()
         const { user } = getUser()
-        const router = useRouter()
         const handleLogout = async () => {
             await logout()
-            await router.push({ name: 'Welcome' })
         }
         
         return { error, handleLogout, user }
--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -5,10 +5,27 @@
 </template>
 
 <script>
+import { watch } from '@vue/runtime-core'
+import { useRouter } from 'vue-router'
 import Navbar from '../components/Navbar.vue'
+import getUser from '../composables/getUser'
 
 export default {
-components: { Navbar }
+  components: { Navbar },
+
+  setup(){
+    const { user } = getUser()
+    const router = useRouter()
+
+    watch(user, () =>{
+      if(!user.value){
+        router.push({ name: 'Welcome' })
+      }
+    })
+
+    return {  }
+  }
+
 }
 </script>
 
