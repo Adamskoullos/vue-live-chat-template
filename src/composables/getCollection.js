a766467557651e27d6d96541600ead186ff1a70a
Adam <adamskoullos@gmail.com>
2021-03-11 16:57:36 +0000

    unsub onSnapshot on un-mount

--- src/composables/getCollection.js
+++ src/composables/getCollection.js
@@ -1,4 +1,5 @@
 import { ref } from "@vue/reactivity"
+import { watchEffect } from "@vue/runtime-core"
 import { fStore } from "../firebase/config"
 
 
@@ -9,7 +10,7 @@ const getCollection = (collection) => {
 
     let collectionRef = fStore.collection(collection).orderBy('createdAt')
 
-    collectionRef.onSnapshot((snap) => {
+    const unsub = collectionRef.onSnapshot((snap) => {
         let results = []
         snap.docs.forEach(doc => {
             doc.data().createdAt && results.push({ ...doc.data(), id: doc.id })
@@ -21,7 +22,14 @@ const getCollection = (collection) => {
         error.value = 'Could not fetch data'
         documents.value = null
     })
+    // unsub method to prevent multiple onSnapshot events running at the same time
+    watchEffect((onInvalidate) => {
+        onInvalidate(() => unsub())
+    })
+
     return { documents, error }
 }
 
 export default getCollection
+
+
